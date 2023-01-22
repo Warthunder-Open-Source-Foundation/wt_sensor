acbe50af050421f5dc09c5374c9785c68782a5ff
FlareFlo <mail@flareflo.dev>
2023-01-23 00:16:51 +0100

    Make scope sets full public

--- src/scoped_range_sets.rs
+++ src/scoped_range_sets.rs
@@ -6,6 +6,6 @@ use bevy_reflect::Reflect;
 #[cfg_attr(feature = "reflect", derive(bevy_reflect::Reflect))]
 #[derive(Debug)]
 pub struct ScopeRangeSets {
-	pub(crate) common: Vec<f64>,
-	pub(crate) boresight_lock: Vec<f64>,
+	pub common: Vec<f64>,
+	pub boresight_lock: Vec<f64>,
 }
