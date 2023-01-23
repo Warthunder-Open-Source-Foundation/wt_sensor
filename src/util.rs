b5fc26a6efcfcc9b8e1199fedb5f47bb155bff41
flareflo <github@flareflo.dev>
2023-01-23 12:41:29 +0100

    Make antenna and limits public

--- src/util.rs
+++ src/util.rs
@@ -6,13 +6,13 @@ use bevy_reflect::Reflect;
 #[cfg_attr(feature = "reflect", derive(bevy_reflect::Reflect, bevy_reflect::FromReflect))]
 #[derive(Debug)]
 pub struct Limits {
-	pub(crate) azimuth: RangeInclusive<f64>,
-	pub(crate) elevation: RangeInclusive<f64>,
+	pub azimuth: RangeInclusive<f64>,
+	pub elevation: RangeInclusive<f64>,
 }
 
 #[cfg_attr(feature = "reflect", derive(bevy_reflect::Reflect, bevy_reflect::FromReflect))]
 #[derive(Debug)]
 pub struct Antenna {
-	pub(crate) angle_half_sens: f64,
-	pub(crate) side_lobes_sensitivity: f64,
+	pub angle_half_sens: f64,
+	pub side_lobes_sensitivity: f64,
 }
